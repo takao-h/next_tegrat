de16925fdebe82cb60b44a31845afb07803587d0
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-10-16 18:04:55 +0900

    update next.js version

--- .next/server/static/development/pages/_document.js
+++ .next/server/static/development/pages/_document.js
@@ -112,7 +112,7 @@ module.exports = __webpack_require__(/*! next/document */ "next/document")
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/ruth/Dev/next_tegrat/node_modules/next/dist/pages/_document.js */"./node_modules/next/dist/pages/_document.js");
+module.exports = __webpack_require__(/*! /Users/k00121/Dev/next_tegrat/node_modules/next/dist/pages/_document.js */"./node_modules/next/dist/pages/_document.js");
 
 
 /***/ }),
